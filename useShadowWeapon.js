a57b32bee7176f6812de3eba779765c36df87875
raydenx <classicroleplaying@gmail.com>
2026-03-21 18:01:01 -0400

    Update useShadowWeapon.js

--- useShadowWeapon.js
+++ useShadowWeapon.js
@@ -1,12 +1,12 @@
 //  get boolean flag 'shadow_weapon'
 //  if not here, then weapon was not "created" yet
 //  warn, exit and cancel attack 
-const version = '0.0.5';
+const version = '0.0.6';
 let rslt = item.getItemBooleanFlags().includes('shadow_weapon');
 if (!rslt) {
 //debugger
   const msg = `Your ${item.name} is not yet created. Cannot attack.`;
-  await item.setItemBooleanFlag('abort');
+  await item.addItemBooleanFlag('abort');
   shared.reject = true;
   return ui.notifications.warn(msg);
 } else {
