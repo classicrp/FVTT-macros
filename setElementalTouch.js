e8cb4139a06b69e640e8573f08b2206be788c220
raydenx <classicroleplaying@gmail.com>
2026-03-23 21:30:15 -0400

    v0.1.4

--- setElementalTouch.js
+++ setElementalTouch.js
@@ -1,4 +1,6 @@
+const version = '0.1.4';
 let element = '', rslt = '';
+//	Get action used
 if ((action.tag === 'acid') || (action.tag === 'cold') || (action.tag === 'fire')) {
   element = action.tag; 
 } else if (action.tag === 'electric') {
@@ -7,6 +9,7 @@ if ((action.tag === 'acid') || (action.tag === 'cold') || (action.tag === 'fire'
   shared.reject = true;
   return;
 }
+//	Get the spell buff associated with said action
 let itmName = `Elemental Touch (${element})`;
 let buff = await actor._itemTypes.buff.filter(b => b.name === n).at(0);
 if (typeof buff === 'undefined') {
@@ -16,16 +19,15 @@ if (typeof buff === 'undefined') {
 }
 
 debugger
+
+//	Get spell details from chatData
 const cl = shared.chatAttacks[0].rollData.cl;  //  item.casterLevel;
 const sl = shared.chatAttacks[0].rollData.sl;  //  item.system.level;
-// const ablMod = actor.system.abilities[`${item.spellbook.ability}`].mod;
 const dc = shared.chatAttacks[0].rollData.dc;  //  10 + sl + ablMod;
-// acid: Attack causes 1 point of ongoing acid damage/rnd for [[floor(@cl/3)]] rounds. The target must make a @Save[fortitude;dc=19] save or be @Condition[sickened;dur=@item.level] during ongoing acid damage.
-// cold: Target must make a @Save[fortitude;dc=18] or be @Condition[fatigued;dur=@cl].  A creature that is already fatigued suffers no additional effect.
-// electric: `Target must make a @Save[fort;dc=${dc}] save or be @Condition[staggered;dur=1].`
-// fire: Your hands ignite and shed light as a torch. Your touch may cause targets to catch on fire.
 let footnote = '', ongoingAcid = '';
+
 if (action.tag === 'acid') {
+	//  Set the 
 	itmName = 'Ongoing Acid Damage';
 	duration = Math.floor(cl / 3);
 	ongoingAcid = await actor._itemTypes.buff.filter(b => b.name === n).at(0);
@@ -34,8 +36,8 @@ if (action.tag === 'acid') {
 		await Item.create(itmData, {parent: actor});
 		ongoingAcid = await actor._itemTypes.buff.filter(b => b.name === itmName).at(0);
 		rslt = await ongoingAcid.setItemDictionaryFlag('duration', duration);
-}
-    footnote = `Causes 1 point of @Apply[${itmName};target] per rnd for [[floor(${cl}/3)]] rnds. The target must make a @Save[fortitude;dc=${dc}] save or be @Condition[sickened;dur=${Math.floor(${cl}/3)}] during ongoing acid damage.`;
+	}
+    footnote = `<p>Causes 1 point of @Apply[${itmName};target] per rnd for [[floor(${cl}/3)]] rnds. The target must make a @Save[fortitude;dc=${dc}] save or be @Condition[sickened;dur=${Math.floor(${cl}/3)}] during ongoing acid damage.</p>`;
 	
 } else if (action.tag === 'cold') {
     footnote = `Target must make a @Save[fortitude;dc=18] or be @Condition[fatigued;dur=@cl].  A creature that is already fatigued suffers no additional effect.`;
@@ -45,7 +47,8 @@ if (action.tag === 'acid') {
     footnote = `Your hands ignite and shed light as a torch. Your touch may cause targets to catch on fire.`;
 }
 
-await buff.setFlag('ckl-roll-bonuses', 'bonus_footnote', footnote); // flags.ckl-roll-bonuses.bonus_footnote
+await shared.chatAttacks[0].effectNotesHTML = footnote;
+// await buff.setFlag('ckl-roll-bonuses', 'bonus_footnote', footnote); // flags.ckl-roll-bonuses.bonus_footnote
 await buff.update({ 'system.level': cl });
 await buff.setActive(true);
 
