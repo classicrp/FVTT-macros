1545ee83c70ef9c80ff0bcf623ee27591342ac79
raydenx <classicroleplaying@gmail.com>
2026-03-28 14:13:07 -0400

    v0.1.5

--- checkPoisonLungtaker.js
+++ checkPoisonLungtaker.js
@@ -1,71 +1,43 @@
-const version = '0.1.4';
+const version = '0.1.5';
 const verbose = true;
 const show = true;
-const saves = 2;
 
+const savesNeeded = 2;
+let chkFinished = false, chkSaved = false, chkDamage = false;
+let rslt = '', newDamage = 0, totDamage = 0;
 let saved = await Number(item.getItemDictionaryFlag('saved'))||0;
 const storDamage = await Number(item.getItemDictionaryFlag('damage'))||0;
-let chkSaved = false, chkDamage = false;
-let rslt = '', newDamage = 0, totDamage = 0;
 
 if (typeof state !== 'undefined' && state) {
 	//	see if there is a save out there
+	if (show) debugger
 	let target = token.document._id;
 	const lm = await game.macros.getName("getChatIdForLastType");
 	const cmsg = await lm.execute({ ctype: 'check' });
 	if (cmsg) {
 		if (show) debugger
+		const roll = cmsg.rolls[0];
 		// negative values so 'remove' stored value
 		newDamage = item.changes.contents[0].value - storDamage;	
-		if (cmsg.rolls[0].isNat20) {
-			//	special success, halve the penalty
-			if (verbose) console.log("critical success");
-			saved++
-			chkSaved = true;
-			if (saved >= saves) {
-				//  finished
-				rslt = await stopPoison();
-				return;
-			} else {
-				chkDamage = true;
-				totDamage = Math.ceil(newDamage / 2) + storDamage;
-			}
-			
-		} else if (cmsg.rolls[0].isNat1){
-			//	special failure, double the penalty
-			if (verbose) console.log("critical failure");
-			chkDamage = true;
-			totDamage = (2 * newDamage) + storDamage;
-			
-		} else if (cmsg.rolls[0].isSuccess) {
-			//	normal success, update the dictionary
-			if (verbose) console.log("success");
-			saved++
-			if (saved >= saves) {
-				//  finished
-				rslt = await stopPoison();
-				return;
-			} else {
-				chkSaved = true;
-				chkDamage = true;
-				totDamage = newDamage + storDamage;
-			}
-			
-		} else if (cmsg.rolls[0].isFailure) {
-			// normal failure, update the penalty
-			if (verbose) console.log("failure");
+		rslt = await checkSave(roll, saved, savesNeeded, newDamage, storDamage);
+		if (rslt) {
+		//	extract results
+			chkFinished = rslt.finished;
+			chkSaved = rslt.saved;
+			chkDamage = rslt.damage;
+			saved = rslt.number;
+			totDamage = rslt.total;
 		}
-		if (chkDamage) await item.setItemDictionaryFlag('damage', totDamage);
-		if (chkSaved) await item.setItemDictionaryFlag('saved', saved);
+	} else {
+		await ui.notifications.warn(`Could not find a recent save for ${token.name}`);
 	}
-	if (show) debugger
 
 } else if (typeof action !== 'unidentified' && action) {	
 	if (action.tag === 'save') {
 		// check the results
 		if (show) debugger
 		let count = 0;
-		async while (count < 10) {
+		while (count < 10) {
 			count++;
 			// Pause for x milliseconds
 			const pauseTime = 1000;
@@ -74,17 +46,84 @@ if (typeof state !== 'undefined' && state) {
 			if (typeof shared.rollData.rolls !== 'undefined') {
 				break;
 			}
-			console.log("count", count);
+			console.log(version, "count:", count);
 		} 
 		//	get local roll values
 	}
 }
+if (chkFinished) {
+	await stopPoison();
+} else {
+	if (chkSaved) await item.setItemDictionaryFlag('saved', saved);
+	if (chkDamage) await item.setItemDictionaryFlag('damage', totDamage);
+}
+
 return
 
 function stopPoison() {
+//	shuts down the buff and zeroes dictionary values
 	if (show) debugger
-	const _active = item.setActive(false);
 	const _saved = item.setItemDictionaryFlag('saved', 0);
 	const _damage = item.setItemDictionaryFlag('damage', 0);
+	const _active = item.setActive(false);
 	return (_active && _saved === 0 && _damage === 0);
+}
+
+function checkSave(r, s, n, nd, sd) {
+//	non-destructive function utilizing passed-in values
+	let a = [], sav = s, totdmg = 0;
+	let cf = false, cd = false, cs = false;
+	if (show) debugger
+	if (r.isNat20) {
+	//	critical success, halve the penalty
+		if (verbose) console.log(version, "Save was a critical success");
+		sav++
+		if (sav >= n) {
+		//  finished
+			cf = true;
+			cd = false;
+			cs = false;
+			totdmg = 0;
+		} else {
+			cf = false;
+			cs = true;
+			cd = true;
+			totdmg = Math.ceil(nd / 2) + sd;
+		}
+		
+	} else if (r.isNat1){
+	//	critical failure, double the penalty
+		if (verbose) console.log(version, "Save was critical failure");
+		cf = false;
+		cs = false;
+		cd = true;
+		totdmg = (2 * nd) + sd;
+		
+	} else if (r.isSuccess) {
+	//	normal success, update the dictionary
+		if (verbose) console.log(version, "Save was a success");
+		sav++
+		if (sav >= n) {
+		//  finished
+			cf = true;
+			cs = true;
+			cd = true;
+			totdmg = Math.ceil(nd / 2) + sd;			
+		} else {
+			cf = false;
+			cs = true;
+			cd = true;
+			totdmg = nd + sd;
+		}
+		
+	} else if (r.isFailure) {
+	// normal failure, update the penalty
+		if (verbose) console.log(version, "Save was a failure");
+		cf = false;
+		cs = false;
+		cd = true;
+		totdmg = nd + sd;
+	}
+	a.push({ ["finshed", cf], ["saved", cs], ["number", sav], ["damage", cd], ["total", totdmg] });
+	return a;
 }
