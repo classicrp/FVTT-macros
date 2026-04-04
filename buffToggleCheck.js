1c2f0ca3a0f65521145ddf77fb40841cb1683ae3
raydenx <classicroleplaying@gmail.com>
2026-04-04 17:11:07 -0400

    v0.0.4
    doh! by changing the duration value I was actually taking 5 + 4 + 3 + 2 + 1 minutes instead of just counting of units.

--- buffToggleCheck.js
+++ buffToggleCheck.js
@@ -1,24 +1,19 @@
-const version = '0.0.3';
+const version = '0.0.4';
 const show = true;
 const verbose = true;
 if (!item.system.active) {
-  if (show) debugger
-  // this will turn off every <frequencyPerUnit> per <frequencyUnits> for <frequencyDuration>.
-  // decrement actual duration and turn back on
-  let units = await Number(item.getItemDictionaryFlag('units'));
-  let dur = await Number(item.getItemDictionaryFlag('frequencyDuration'));
-  units++;
-  if (units <= dur) {
-    await item.setItemDictionaryFlag('units', units);
-    if (verbose) console.log(version, "units:", units);
-    const duration = deepClone(item.system.duration);
-    duration.value = String(dur - units);
-    await item.update({ 'system.duration': duration });
-    await item.setActive(true);
-  } else {
-    // we are done
-  }
-
-  
-  if (verbose) console.log(version);
+	if (show) debugger
+	// this will turn off every <frequencyPerUnit> per <frequencyUnits> for <frequencyDuration>.
+	// increment the units and turn back on
+	let units = await Number(item.getItemDictionaryFlag('units'));
+	let dur = await Number(item.getItemDictionaryFlag('frequencyDuration'));
+	units++;
+	if (units <= dur) {
+		await item.setItemDictionaryFlag('units', units);
+		if (verbose) console.log(version, "units:", units);
+		await item.setActive(true);
+	} else {
+		// we are done
+		await item.setItemDictionaryFlag('units', 0);
+	} 
 }
