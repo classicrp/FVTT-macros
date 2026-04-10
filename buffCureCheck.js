e8249282161a49986e18870585ff6ae9a4ab28b0
raydenx <classicroleplaying@gmail.com>
2026-04-10 12:26:25 -0400

    v0.0.3
    .delete needs () at end.

--- buffCureCheck.js
+++ buffCureCheck.js
@@ -1,11 +1,11 @@
-const version = '0.0.2';
+const version = '0.0.3';
 const show = true;
 const verbose = true;
 
 	if (typeof action !== 'undefined') {
 		if (action.tag === 'cure') {
 			//  buff needs to be removed
-			await item.delete;
+			await item.delete();
 		}
 	}
 return
