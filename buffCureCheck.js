7ae888df56a298d2fc753ea4b4b1eb683f1e8aef
raydenx <classicroleplaying@gmail.com>
2026-04-10 12:22:01 -0400

    v0.0.2
    missed .tag for action check

--- buffCureCheck.js
+++ buffCureCheck.js
@@ -1,9 +1,9 @@
-const version = '0.0.1';
+const version = '0.0.2';
 const show = true;
 const verbose = true;
 
 	if (typeof action !== 'undefined') {
-		if (action === 'cure') {
+		if (action.tag === 'cure') {
 			//  buff needs to be removed
 			await item.delete;
 		}
