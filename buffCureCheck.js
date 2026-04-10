69bbe77941ea7d5c6880011572eea99eb1e352cd
raydenx <classicroleplaying@gmail.com>
2026-04-10 12:17:37 -0400

    v0.0.1
    remove (de)buff on "cure",

---
+++ buffCureCheck.js
@@ -0,0 +1,11 @@
+const version = '0.0.1';
+const show = true;
+const verbose = true;
+
+	if (typeof action !== 'undefined') {
+		if (action === 'cure') {
+			//  buff needs to be removed
+			await item.delete;
+		}
+	}
+return
