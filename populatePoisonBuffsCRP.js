c69ac7f525483e9e9553a8293f32319a7233db00
raydenx <classicroleplaying@gmail.com>
2026-04-10 17:04:44 -0400

    v0.0.1
    created shell outline how to proceed.

---
+++ populatePoisonBuffsCRP.js
@@ -0,0 +1,47 @@
+const version = '0.0.1';
+const show = false;
+const verbose = true;
+const paused = true;
+
+//	TEST CASE "Dragon bile"
+
+//	GET Poison items from non-CRP Compendium packs.
+
+//	CREATE a copy of Poison item in "Compendium.crp-contents.crp-items" in folder "ITEMS", subfolder "Poisons" for each not already there.
+	//	SET <Unidentified Name> to "Vial of liquid".
+	//	SET <Superficial Details> to "Some liquid in a vial."
+	//	GET <Identified Properties>
+	//		ADD at top "<h3>" + <Item.name> + "</h3>"
+	//		INSERT after "Cure..." - "</p>" + "; <b>Value</b> " + <price> + " gp.</p>"
+	//	SET <effectNotes> = "<span style="font-size:1.2em"><b>Effect:</b> + effect from details + @Apply[ (place uuid for the poison's buff here)]<br> + 
+	//		IF a secondary item exists add "<b>Secondary:</b> " + 
+	//		IF a Condition exists, add; "@Condition[ (condition lowercase name)" + ";duration=" + Set duration as a die roll/number only for "rnds"
+	//				content (if you want it to last random "m" minutes, "t" turns, "h" hours, "d" days, then multilpy by
+	//				10 for "m",
+	//				100 for "t", 
+	//				600 for "h" 
+	//				14400 for "d" )
+	//				OR number + "m" or "t" or "h" or "d" + "]</span>"
+	//	SET <action['Use'].SavingThrowEffect> = <span style="font-size:1.2em"><b>Frequency:</b> " + (frequency from details) + "<br><b>Cure:</b> " + 
+	//		(cure from details OR 1 if none exists there) + " save(s)</span>"
+	
+//	CREATE a new BUFF item placed in "Compendium.crp-contents.crp-items" in folder "BUFFS", subfolder "Poisons"
+	//	SET uuid of associated Poison Item to newly created BUFF uuid.
+	//	COPY over <details> from Poison Item and write to <Identified Properties>.
+	//	SET "on-use" macro "buffCureCheck" to "Compendium.crp-contents.crp-macros.Macro.wEGLTOmr7iSa5E3l"
+	//	SET "on-toggle" macro "buffToggleCheck" to "Compendium.crp-contents.crp-macros.Macro.0kwyj53zVj6I6rKs"
+	//	CREATE a new <changes> object for each type of damage listed in Details
+	//		SET <target> to damage type (mostly an ability)
+	//		SET <formula> to number or in case of dice; "-floor(random() * [ size of dice ] + 1) +@dFlags.poison(poison name).(target)
+	//		ENSURE <operator> is "add"
+	//		LOOP as needed
+	//	CREATE dictionary items for;
+	//		<frequencyUnits> (String) {"" for inifinity, "round" for rnds, "turn" for turns, "hour" for hrs, "day" for days}, pulled from "Details".
+	//		<frequencyDuration> (Number), pulled from "Details".
+	//		<consecutiveSaves> (Number) { -1 if not present, 0 otherwise }, pulled from "Details".
+	//		<savesNeeded> (Number) { 1 if not present, ohterwise pulled from "Details"}.
+	//		<savesMade> (Number) { 0 }.
+	//		<unitsPassed> (Number) { 0 }.
+	//		<target> (Number) { 0 }, one for each entry in <changes> above.
+	
+	
