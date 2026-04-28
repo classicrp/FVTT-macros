7df337ef4aa5971cf5a13ff4f197acfd9ab88ded
raydenx <classicroleplaying@gmail.com>
2026-04-28 11:57:57 -0400

    Update buffCureCheck.js
    - turned off debug info.
    - added Boolean flag "cured" to inform item scripts that the item is being deleted.

--- buffCureCheck.js
+++ buffCureCheck.js
@@ -1,10 +1,11 @@
-const version = '0.0.3';
-const show = true;
-const verbose = true;
+const version = '0.1.0';
+const show = false;
+const verbose = false;
 
 	if (typeof action !== 'undefined') {
 		if (action.tag === 'cure') {
 			//  buff needs to be removed
+			await item.addItemBooleanFlag('cured');
 			await item.delete();
 		}
 	}
