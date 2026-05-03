d5ee88277d47b6d1ededcd95cdb115c92d6f7584
raydenx <classicroleplaying@gmail.com>
2026-05-03 15:57:28 -0400

    v0.1.6
    - Using <rolls>... .options.type to look for "crit" specific lines and keep them (i.e for "Burst" weapons).

--- bareCriticals.js
+++ bareCriticals.js
@@ -1,5 +1,5 @@
-const _VERSION = '0.1.5';
-const _SHOW = false;
+const _VERSION = '0.1.6';
+const _SHOW = true;
 const _HEAD = `Macro.bareCriticals(${_VERSION})`;
 
 /*	Changes chat output for an attack by removing all formula data that is
@@ -17,6 +17,7 @@ const ATTR_FRML = "_formula";
 const ATTR_TRMS = "terms";
 const ATTR_TRMS_TOT = "terms.0.total";
 const ATTR_TOT = "_total";
+const ATTR_OPT_TYP = "options.type";
 
 let rslt = null;
 let srcs = await foundry.utils.getProperty(shared, ATTR_CHAT_ATZ_ATK);
@@ -26,22 +27,29 @@ if (_SHOW) debugger
 let rolls = srcs[ATTR_CRIT_DMG][ATTR_ROLLS];
 const len = rolls.length;
 for (let i = 0; i < len; i++) {
-	let rolled = 0, length = 0, fltrd = null;
+	let rolled = 0, length = 0, fltrd = [];
 	let r = rolls[i];
-	//	Match <rolls> formula to only include die expression
-	if (r[ATTR_FRML].includes('sizeRoll')) {
-		//	sizeRoll must be present, [Roll] is optional
-		const RGX_SIZRL = /\bsizeRoll\([\d,]+\)(?:\[Roll\])/i;
-		fltrd = r[ATTR_FRML].match(RGX_SIZRL);
+	let type = foundry.utils.getProperty(r, ATTR_OPT_TYP);
+	if (type === "crit") {
+		//	"crit" line specifically, keep as is
+		await fltrd.push(r[ATTR_FRML]);
 	} else {
-		//	[Roll] must be present for this capture
-		const RGX_NRML = /\b(?:\d+d\d+|\d+)\[Roll\]/i;
-		fltrd = r[ATTR_FRML].match(RGX_NRML);
+		//	Match <rolls> formula to only include die expression
+		if (r[ATTR_FRML].includes('sizeRoll')) {
+			//	sizeRoll must be present, [Roll] is optional
+			const RGX_SIZRL = /\bsizeRoll\([\d,]+\)(?:\[Roll\])/i;
+			fltrd = r[ATTR_FRML].match(RGX_SIZRL);
+		} else {
+			//	[Roll] must be present for this capture
+			const RGX_NRML = /\b(?:\d+d\d+|\d+)\[Roll\]/i;
+			fltrd = r[ATTR_FRML].match(RGX_NRML);
+		}
 	}
 	if (isEmpty(fltrd)) {
 		await delete rolls[i];
 		continue;
 	}
+
 	//	Change <rolls> formula to that of dice rolls only
 	rslt = await foundry.utils.setProperty(r, ATTR_FRML, fltrd.at(0));
 	length = r[ATTR_TRMS].length;
