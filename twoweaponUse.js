f04a510334f0507307725b4997f3b550d284bb43
raydenx <classicroleplaying@gmail.com>
2026-05-03 00:38:16 -0400

    v0.2.8
    - Pulled in both weapon items.
    - Activate by weapons[n].use().
    - Used foundry.utils.getProperty() to pull <flags> data.

--- twoweaponUse.js
+++ twoweaponUse.js
@@ -1,17 +1,26 @@
 // async function anonymous(speaker,actor,token,character,scope,item,shared,action,state,startTime
-const version = '0.2.7';
-debugger
+const _VERSION = '0.2.8';
+const _SHOW = true;
+
+let rslt = "";
+
+if (_SHOW) debugger
 //	game.actors.get('tPis1bRmFuPFq4Gw')._itemTypes.buff.filter(b => b.name === 'Two Weapon Use').at(0)
 	const buff = actor._itemTypes.buff.filter(b => b.name === 'Two Weapon Use').at(0);
 	if (!buff.isActive) return false;
 	let used = Number(buff.getItemDictionaryFlag('used'));
 	used++;
 	if (used < 2) {
-		firstW = buff.flags['ckl-roll-bonuses'].target_weapon[0];
-		secondW = buff.flags['ckl-roll-bonuses'].target_weapon[1];
-		weapon = (item.id === firstW) ? actor.items.contents.filter(a => a.id === secondW).at(0).name : actor.items.contents.filter(a => a.id === firstW).at(0).name;
-		await useAction(weapon);
+		const firstW = await foundry.utils.getProperty(buff, 'flags.ckl-roll-bonuses.target_weapon.0');
+		const secondW = await foundry.utils.getProperty(buff, 'flags.ckl-roll-bonuses.target_weapon.1');
+		const weapons = await actor.items.contents.filter(a => a.id === firstW).concat(actor.items.contents.filter(a => a.id === secondW));
 		await buff.setItemDictionaryFlag('used', used);
+		if (item.id === firstW) {
+			rslt = weapons[1].use();
+		} else {
+			rslt = weapons[0].use();
+		}
+//		await useAction(weapon);
 		return true;
 	} else {
 	//	second weapon attack, rest count to zero	
