afbfdccec66d83bf42dd8a56d4ce41fe7fcdf2a0
raydenx <classicroleplaying@gmail.com>
2026-05-05 20:50:39 -0400

    v0.3.5
    Major format overhaul

--- elementalFist_v0.3.4.js
+++ elementalFist_v0.3.4.js
@@ -1,18 +1,34 @@
-//	version: 0.3.4
-const show = true;
-let src = "", rslt = "", skip = true;
+const _VERSION = '0.3.5';
+const _HEAD = "Macro.elementalFist v" + _VERSION;
+const _SHOW = true;
+const _VERBOSE = true;
+
 const act = action.tag;
 const name = action.name;
 const note = ['@Use[Elemental Fist#Stop]'];
+let src = "", rslt = "", skip = true;
+
+const ATTR_FLGS_CRB_BCM = "flags.ckl-roll-bonuses.bonus_conditional-modifiers";
+const ATTR_FLGS_CRB_BFN = "flags.ckl-roll-bonuses.bonus_footnote";
+const ATTR_FLGS_CRB = "flags.ckl-roll-bonuses";
+const ATTR_FLG_CRB = "ckl-roll-bonuses";
+const ATTR_FLG_BCM = "bonus_conditional-modifiers";
+const ATTR_FLG_BFN = "bonus_footnote";
+const ATTR_SYS_ENT = "system.effectNotes";
+const ATTR_BFLG_EFST = "elemental_fist";
+const ATTR_DFLG_NRG = "energy";
+
+const CLSS_MNK_LVL = "@classes.monk.level";
+const CLSS_GSTL_LVL = "@classes.gestalt.level";
 
 if (act !== "clear") {
 	const mCl = actor.classes;
 	//  only Monk can use
 	if (typeof mCl.monk !== "undefined") {
-		rslt = "@classes.monk.level";
+		rslt = CLSS_MNK_LVL;
 	} else if (typeof mCl.gestalt !== "undefined") {
 		if (mCl.gestalt.name.toLowerCase().includes('monk')) {
-			rslt = "@classes.gestalt.level";
+			rslt = CLSS_GSTL_LVL;
 		}
 	} else {
 		return;
@@ -31,107 +47,100 @@ if (act !== "clear") {
 		}],
 		name: `${name}`
 	};
-	if (show) console.log("new conditional:", cond);
+	if (_VERBOSE) console.log("new conditional:", cond);
 
-	rslt = await item.addItemBooleanFlag("elemental_fist");
-	if (show) console.log('boolean flag "elemental_fist" added:', rslt);
+	rslt = await item.addItemBooleanFlag(ATTR_BFLG_EFST);
+	if (_VERBOSE) console.log(`boolean flag "${ATTR_BFLG_EFST}" added:`, rslt);
 
-	rslt = await item.addItemBooleanFlag("bonus_conditional-modifiers");
-	if (show) console.log('boolean flag "bonus_conditional-modifiers" added:', rslt);
+	rslt = await item.addItemBooleanFlag(ATTR_FLG_BCM);
+	if (_VERBOSE) console.log(`boolean flag "${ATTR_FLG_BCM}" added:`, rslt);
 
-	rslt = await item.addItemBooleanFlag("bonus_footnote");
-	if (show) console.log('boolean flag "bonus_footnote" added:', rslt);
+	rslt = await item.addItemBooleanFlag(ATTR_FLG_BFN);
+	if (_VERBOSE) console.log(`boolean flag "${ATTR_FLG_BFN}" added:`, rslt);
 
-	rslt = await item.setItemDictionaryFlag("energy", act);
-	if (show) console.log(`dictionary flag "energy" added "${act}":`, rslt);
+	rslt = await item.setItemDictionaryFlag(ATTR_DFLG_NRG, act);
+	if (_VERBOSE) console.log(`dictionary flag "${ATTR_DFLG_NRG}" added "${act}":`, rslt);
 
-	src = await deepClone(item.flags['ckl-roll-bonuses']);
+	src = await deepClone(item.flags[ATTR_FLG_CRB]);
 	if (typeof src !== "undefined") {
 
 		/*	TARGET WEAPONS  */
-		if (show) debugger
+		if (_SHOW) debugger
 		for (i=0; i < src.target_weapon.length; i++) {
 			let weap = actor.items.get(src.target_weapon[i]);
-			if (show) console.log(weap);
-			rslt = await weap.update({ ['system.effectNotes']: note });
-			if (show) console.log(rslt, weap);
+			if (_VERBOSE) console.log(weap);
+			rslt = await weap.update({ [ATTR_SYS_ENT]: note });
+			if (_VERBOSE) console.log(rslt, weap);
 		}
 		
 		/*	BONUS CONDITIONAL MODIFIERS	 */
 		const bcm = {
-			["bonus_conditional-modifiers"]: []
+			[ATTR_FLG_BCM]: []
 		};
 		const fn = `<em>Elemental Fist (<strong>${act}</strong>) active.</em>`;
 		
-		rslt = await bcm['bonus_conditional-modifiers'].push(cond);
-		if (show) console.log(`set conditional modifier to be added":`, bcm);
+		rslt = await bcm[ATTR_FLG_BCM].push(cond);
+		if (_VERBOSE) console.log(`set conditional modifier to be added":`, bcm);
         
-		if (typeof src['bonus_conditional-modifiers'] !== "undefined") {
+		if (typeof src[ATTR_FLG_BCM] !== "undefined") {
           //  it is already there
-			src['bonus_conditional-modifiers'].push(cond);        
-			src['bonus_footnote'] = fn;
+			src[ATTR_FLG_BCM].push(cond);        
+			src[ATTR_FLG_BFN] = fn;
 		
 		} else {
 			// trouble
-			if (show) console.log('Little Trouble - no "bonus_conditional-modifiers"');
+			console.warn(_HEAD, `Little Trouble - no ${ATTR_FLG_BCM}`);
         }
-        rslt = await item.update({ [`flags.ckl-roll-bonuses`]: src });
-		if (show) console.log(`updated item document:`, rslt);
+        rslt = await item.update({ [ATTR_FLGS_CRB]: src });
+		if (_VERBOSE) console.log(`updated item document:`, rslt);
 		
 	} else {
 		//	trouble
-		if (show) console.log('BIG TROUBLE - no src');
+		console.error(_HEAD, 'BIG TROUBLE - no src');
+		return;
 	}
 } else {
 	
-	src = await deepClone(item.flags['ckl-roll-bonuses']);
+	src = await deepClone(item.flags[ATTR_FLG_CRB]);
 	if (typeof src !== "undefined") {
 	
-		rslt = await item.removeItemBooleanFlag("elemental_fist");
-		if (show)
-			console.log(`boolean flag "elemental_fist" removed:`, rslt);
+		rslt = await item.removeItemBooleanFlag(ATTR_BFLG_EFST);
+		if (_VERBOSE) console.log(`boolean flag "${ATTR_BFLG_EFST}" removed:`, rslt);
 
-		rslt = await item.removeItemBooleanFlag("bonus_conditional-modifiers");
-		if (show)
-			console.log('boolean flag "bonus_conditional-modifiers" removed:', rslt);
+		rslt = await item.removeItemBooleanFlag(ATTR_FLG_BCM);
+		if (_VERBOSE) console.log(`boolean flag ${ATTR_FLG_BCM} removed:`, rslt);
 
-		rslt = await item.removeItemBooleanFlag("bonus_footnote");
-		if (show)
-			console.log('boolean flag "bonus_footnote" removed:', rslt);
+		rslt = await item.removeItemBooleanFlag(ATTR_FLG_BFN);
+		if (_VERBOSE) console.log(`boolean flag "${ATTR_FLG_BFN}" removed:`, rslt);
 
-		rslt = await item.setItemDictionaryFlag("energy", "");
-		if (show)
-			console.log(`dictionary flag "energy" cleared:`, rslt);
+		rslt = await item.setItemDictionaryFlag(ATTR_DFLG_NRG, "");
+		if (_VERBOSE) console.log(`dictionary flag "${ATTR_DFLG_NRG}" cleared:`, rslt);
 
 		/*	TARGET WEAPONS  */
 		for (i=0; i < src.target_weapon.length; i++) {
 			let weap = actor.items.get(src.target_weapon[i]);
-			if (show) console.log(weap);
-			rslt = await weap.update({ ['system.effectNotes']: [] });
-			if (show) console.log(rslt, weap);
+			if (_VERBOSE) console.log(weap);
+			rslt = await weap.update({ [ATTR_SYS_ENT]: [] });
+			if (_VERBOSE) console.log(rslt, weap);
 		}
 
 
-		rslt = await item.update({	[`flags.ckl-roll-bonuses.bonus_footnote`]: "" });
-		if (show)
-			console.log(`removed footnote:`, rslt);
+		rslt = await item.update({	[ATTR_FLGS_CRB_BFN]: "" });
+		if (_VERBOSE) console.log(`removed footnote:`, rslt);
 
-		src = Array.from(item.flags["ckl-roll-bonuses"]["bonus_conditional-modifiers"]);
-		if (show)
-			console.log("current array of Roll Bonuses conditional modifers:", src);
+		src = Array.from(item.flags[ATTR_FLG_CRB][ATTR_FLG_BCM]);
+		if (_VERBOSE) console.log("current array of Roll Bonuses conditional modifers:", src);
 
 		for (let i = 0; i < src.length; i++) {
 			rslt = await src.pop();
-			if (show)
-				console.log("removed conditional from array:", rslt);
+			if (_VERBOSE) console.log("removed conditional from array:", rslt);
 			skip = false;
 		}
 		if (!skip) {
 			rslt = await item.update({
-				[`flags.ckl-roll-bonuses.bonus_conditional-modifiers`]: src
+				[ATTR_FLGS_CRB_BCM]: src
 			});
-			if (show)
-				console.log('updated the "item" sheet:', rslt);
+			if (_VERBOSE) console.log('updated the "item" sheet:', rslt);
 		}
 	}
 }
